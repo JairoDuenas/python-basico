41c1482745ed428989612601d4c763919abdbcde
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-15 17:03:39 -0500

    Bucle while [continue, pass, else]

---
+++ 18_bucle_while_continue.py
@@ -0,0 +1,37 @@
+# continue
+
+""" for letra in 'Python':
+  if letra == 'h':
+    continue
+  print(f'Viendo la letra {letra}') """
+
+""" nombre = 'Píldoras informáticas'
+
+contador = 0
+
+for i in nombre:
+  if i == ' ':
+    continue
+  contador += 1
+
+print(contador) """
+
+# pass
+""" while True:
+  pass
+class Mi_clase:
+  pass # para implementar más tarde """
+
+# else
+
+email = input('Ingrese tu email: ')
+
+for i in email:
+  if i == '@':
+    arroba = True
+    break
+
+else:
+  arroba = False
+print(arroba)
+
