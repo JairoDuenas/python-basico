89f76b81754db38ce2f6db6c7da929bbe49f321d
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-15 07:23:22 -0500

    Funciones con parámetros

---
+++ 06_funciones_1.py
@@ -0,0 +1,14 @@
+# función sin parámetros
+def suma():
+  num1 = 5
+  num2 = 7
+  print(num1 + num2)
+suma()
+
+# función con parámetros
+def suma(num1, num2):
+  resultado = num1 + num2
+  return resultado
+almacena_resultado = suma(5, 7)
+print(almacena_resultado)
+
