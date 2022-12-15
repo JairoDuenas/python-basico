b2b9a6eb78c5415f71bff21c0dcb52aee292ba87
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-15 14:18:34 -0500

    Bucle for [contador, range]

---
+++ 15_bucle_for_2.py
@@ -0,0 +1,24 @@
+""" lista = ['pildoras', 'informática', 3]
+
+for i in lista:
+  print('Hola', end='-') """
+
+""" for i in 'jair@hotmail.com':
+  print(i, end='_') """
+
+""" contador = 0
+mi_email = input('Ingrese su dirección de email: ')
+
+for i in mi_email:
+  if(i == '@' or i == '.'):
+    contador = contador + 1
+
+if contador==2:
+  print('Email es correcto')
+else:
+  print('El email no es correcto')
+ """
+
+for i in range(5):
+  print('Hola', i)
+
