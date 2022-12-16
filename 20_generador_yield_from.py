a669bea6e2c849d48e21e8fdfcb17b2c402934ba
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-16 06:54:47 -0500

    Generadores [yield from]

---
+++ 20_generador_yield_from.py
@@ -0,0 +1,9 @@
+def devuelve_ciudades(*ciudades):
+  for elemento in ciudades:
+    #for sub_elemento in elemento:
+      yield from elemento
+
+ciudades_devueltas = devuelve_ciudades('Madrid', 'Barcelona', 'Bilbao', 'Valencia')
+
+print(next(ciudades_devueltas))
+print(next(ciudades_devueltas))
