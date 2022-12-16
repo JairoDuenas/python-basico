6f943de28419151f6e6d7b892515f40ddb886e20
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-16 17:47:44 -0500

    Paquetes distribuibles

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='paqute_calculo',
+  version='1.0',
+  description='Paquete de redondeo y potencia',
+  author='Jairo',
+  author_email='jairo@gmail.com',
+  url='www.jairodv.com',
+  packages=['paquetes', 'paquetes.redondeo_potencia']
+)
