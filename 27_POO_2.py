b3c43ddd5c9c8d204f944803606192d686d27dd6
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-16 09:25:14 -0500

    POO [encapsulamiento]

--- 27_POO_2.py
+++ 27_POO_2.py
@@ -2,21 +2,21 @@ class Coche():
 
   # Constructor
   def __init__(self):
-    self.largo_chasis = 250
-    self.ancho_chasis = 120
-    self.ruedas = 4
-    self.en_marcha = False
+    self.__largo_chasis = 250
+    self.__ancho_chasis = 120
+    self.__ruedas = 4
+    self.__en_marcha = False
 
   def arrancar(self, arrancamos):
     self.en_marcha = arrancamos
 
-    if(self.en_marcha):
+    if(self.__en_marcha):
       return 'El coche está en marcha'
     else:
       return 'El coche está parado'
 
   def estado(self):
-    print(f'El coche tiene {self.ruedas} ruedas. Un ancho de {self.ancho_chasis} y un largo de {self.largo_chasis}')
+    print(f'El coche tiene {self.__ruedas} ruedas. Un ancho de {self.__ancho_chasis} y un largo de {self.__largo_chasis}')
 
 # Instanciar una clase
 mi_coche = Coche()
@@ -27,4 +27,5 @@ print('------- Se crea el segundo objeto --------')
 
 mi_coche2 = Coche()
 print(mi_coche2.arrancar(False))
+mi_coche2.__ruedas = 2 # no se modifica
 mi_coche2.estado()
