b9ebf604f896bca79060b3217ec3a3357758706c
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-16 09:50:21 -0500

    POO [encapsulamiento de métodos]

--- 27_POO_2.py
+++ 27_POO_2.py
@@ -8,15 +8,34 @@ class Coche():
     self.__en_marcha = False
 
   def arrancar(self, arrancamos):
-    self.en_marcha = arrancamos
+    self.__en_marcha = arrancamos
 
     if(self.__en_marcha):
+      chequeo = self.__chequeo_interno()
+
+    if(self.__en_marcha and chequeo):
       return 'El coche está en marcha'
+
+    elif(self.__en_marcha and chequeo==False):
+      return 'Algo ha salido mal en el chequeo. No se puede arrancar'
     else:
       return 'El coche está parado'
 
   def estado(self):
     print(f'El coche tiene {self.__ruedas} ruedas. Un ancho de {self.__ancho_chasis} y un largo de {self.__largo_chasis}')
+
+# encapsulado del método (__chequeo_interno())
+  def __chequeo_interno(self):
+    print('Realizando chequeo interno')
+
+    self.gasolina = 'ok'
+    self.aceite = 'ok'
+    self.puertas = 'cerradas'
+
+    if(self.gasolina == 'ok' and self.aceite == 'ok' and self.puertas == 'cerradas'):
+      return True
+    else:
+      return False
 
 # Instanciar una clase
 mi_coche = Coche()
@@ -27,5 +46,4 @@ print('------- Se crea el segundo objeto --------')
 
 mi_coche2 = Coche()
 print(mi_coche2.arrancar(False))
-mi_coche2.__ruedas = 2 # no se modifica
 mi_coche2.estado()
