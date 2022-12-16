ab0026e3668c73738873f44db6b0e2d313e038e6
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-16 11:35:26 -0500

    Herencia [herencia múltiple]

--- 29_herencia.py
+++ 29_herencia.py
@@ -19,6 +19,14 @@ class Vehiculos():
     def estado(self):
         print(f'Marca: {self.marca} \nModelo: {self.modelo} \nEn marcha: {self.en_marcha} \nAcelerando: {self.acelera} \nFrenado: {self.frena}')
 
+class Furgoneta(Vehiculos):
+    def carga(self, cargar):
+        self.cargado = cargar
+
+        if(self.cargado):
+            return 'La furgoneta está cargada'
+        else:
+            return 'La furgoneta o está cargada'
 
 class Moto(Vehiculos):
     hace_caballito = ''
@@ -28,7 +36,23 @@ class Moto(Vehiculos):
     def estado(self):
         print(f'Marca: {self.marca} \nModelo: {self.modelo} \nEn marcha: {self.en_marcha} \nAcelerando: {self.acelera} \nFrenado: {self.frena} \n{self.hace_caballito}')
 
+class V_electrico():
+    def __init__(self):
+        self.autonimia = 100
+    def cargar_energia(self):
+        self.cargando = True
 
 mi_moto = Moto('Honda', 'CBR')
 mi_moto.caballito()
 mi_moto.estado()
+
+mi_furgoneta = Furgoneta('Renault', 'Kangoo')
+mi_furgoneta.arrancar()
+mi_furgoneta.estado()
+print(mi_furgoneta.carga(True))
+
+
+class Bici_electrica(V_electrico, Vehiculos):
+    pass
+
+mi_bici = Bici_electrica()
