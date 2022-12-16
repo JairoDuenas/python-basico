26dccac0c62ea9fa9e9e31839fa662dd52553183
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-16 11:10:03 -0500

    Herencia

--- 29_herencia.py
+++ 29_herencia.py
@@ -21,15 +21,7 @@ class Vehiculos():
 
 
 class Moto(Vehiculos):
-    hace_caballito = ''
-
-    def caballito(self):
-        self.hace_caballito = 'Voy haciendo caballito'
-
-    def estado(self):
-        print(f'Marca: {self.marca} \nModelo: {self.modelo} \nEn marcha: {self.en_marcha} \nAcelerando: {self.acelera} \nFrenado: {self.frena} \nHace caballito: {self.hace_caballito}')
-
+    pass
 
 mi_moto = Moto('Honda', 'CBR')
-mi_moto.caballito()
 mi_moto.estado()
