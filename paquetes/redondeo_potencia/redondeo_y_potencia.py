f681cbb452c52f0fc2ed16f7fe50a1b41397a85c
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-16 17:14:22 -0500

    Paquetes

---
+++ paquetes/redondeo_potencia/redondeo_y_potencia.py
@@ -0,0 +1,5 @@
+def potencia(base, exponente):
+  print(f'La potencia del número es: {base ** exponente}')
+
+def redondear(numero):
+  print(f'El resultado de la suma es: {round(numero)}')
---
+++ paquetes/uso_modulos.py
@@ -0,0 +1,7 @@
+from basicos.operaciones_basicas import dividir, sumar
+from redondeo_potencia.redondeo_y_potencia import potencia, redondear
+
+dividir(4, 2)
+sumar(10, 5)
+potencia(2, 3)
+redondear(4.8)
