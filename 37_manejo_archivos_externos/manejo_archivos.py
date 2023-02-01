f3512cbcc870cfc969465d9f2d997a06b0f23c73
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-01 09:20:02 -0500

    Archivos texto 2

--- 37_manejo_archivos_externos/manejo_archivos.py
+++ 37_manejo_archivos_externos/manejo_archivos.py
@@ -23,6 +23,25 @@ print(lineas_texto[0])
 print(lineas_texto[1]) """
 
 ### Agregar más líneas al texto ###
-archivo_texto = open('archivo.txt', 'a')
+""" archivo_texto = open('archivo.txt', 'a')
 archivo_texto.write('\n siempre es una buena ocación para estudiar Python')
+archivo_texto.close() """
+
+### Archivos externos II ###
+""" archivo_texto = open('archivo.txt', 'r')
+archivo_texto.seek(len(archivo_texto.read())/2)
+print(archivo_texto.read()) """
+
+""" archivo_texto = open('archivo.txt', 'r')
+archivo_texto.seek(len(archivo_texto.readline()))
+print(archivo_texto.read()) """
+
+archivo_texto = open('archivo.txt', 'r+')
+#print(archivo_texto.readlines())
+
+lista_texto = archivo_texto.readlines()
+lista_texto[1] = 'Esta línea ha sido incluida desde el exterior \n'
+
+archivo_texto.seek(0)
+archivo_texto.writelines(lista_texto)
 archivo_texto.close()
