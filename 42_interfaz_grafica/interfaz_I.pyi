a07c1edeb4e2fe73302c8fbbf5c870e84e17821c
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-01 15:02:53 -0500

    Interfaz gráfica 1

---
+++ 42_interfaz_grafica/interfaz_I.pyi
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+root.title('Ventana de prueba')
+root.resizable(True, False)
+root.iconbitmap('logo.ico')
+root.geometry('550x350')
+root.config(bg='yellow')
+root.mainloop()
