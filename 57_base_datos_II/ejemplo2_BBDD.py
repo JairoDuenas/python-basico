848173f80f917c2f96a884e64716319a2fc02628
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-03 12:21:23 -0500

    Clausula [UNIQUE], CRUD

--- 57_base_datos_II/ejemplo2_BBDD.py
+++ 57_base_datos_II/ejemplo2_BBDD.py
@@ -4,10 +4,10 @@ mi_conexion=sqlite3.connect('GestionProductos')
 
 mi_cursor=mi_conexion.cursor()
 
-mi_cursor.execute('''
+""" mi_cursor.execute('''
   CREATE TABLE PRODUCTOS (
   ID INTEGER PRIMARY KEY AUTOINCREMENT,
-  NOMBRE_ARTICULO VARCHAR(50),
+  NOMBRE_ARTICULO VARCHAR(50) UNIQUE,
   PRECIO INTEGER,
   SECCION VARCHAR(20))
 ''')
@@ -19,6 +19,19 @@ productos=[
   ('jarrón', 45, 'cerámica')
 ]
 mi_cursor.executemany("INSERT INTO PRODUCTOS VALUES (NULL,?,?,?)", productos)
+ """
+
+# consultar productos
+""" mi_cursor.execute("SELECT * FROM PRODUCTOS WHERE SECCION='confección'")
+
+productos=mi_cursor.fetchall()
+print(productos) """
+
+# actualizar (UPDATE)
+""" mi_cursor.execute("UPDATE PRODUCTOS SET PRECIO=35 WHERE NOMBRE_ARTICULO='pelota'") """
+
+# borrar (DELETE)
+mi_cursor.execute("DELETE FROM PRODUCTOS WHERE ID=4")
 
 mi_conexion.commit()
 
