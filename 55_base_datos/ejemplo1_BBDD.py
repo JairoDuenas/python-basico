0a8bb2e781ab011c189d43dd21d09f1fecd66be6
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-03 10:31:36 -0500

    Insertar varios productos [mostrar información de la base de datos]

--- 55_base_datos/ejemplo1_BBDD.py
+++ 55_base_datos/ejemplo1_BBDD.py
@@ -12,7 +12,25 @@ mi_cursor=mi_conexion.cursor()
 # 4. Manejar los resultados de la query(consulta)
 """1. Insertar, Leer, Actualizar, Borrar
     (Create, Read, Update, Delete)"""
-mi_cursor.execute("INSERT INTO PRODUCTOS VALUES('BALÓN', 15, 'DEPORTES')")
+""" mi_cursor.execute("INSERT INTO PRODUCTOS VALUES('BALÓN', 15, 'DEPORTES')")"""
+
+# ---- insertar varios datos --------------
+""" varios_productos=[
+    ('Camiseta', 10, 'Deportes'),
+    ('Jarrón', 90, 'Cerámica'),
+    ('Camión', 20, 'Juguetes'),
+]
+
+mi_cursor.executemany('INSERT INTO PRODUCTOS VALUES (?,?,?)', varios_productos)
+ """
+
+# ---- ver información de la base de datos --------------
+mi_cursor.execute('SELECT * FROM PRODUCTOS')
+variosProductos=mi_cursor.fetchall()
+
+for producto in variosProductos:
+    #print(producto)
+    print('Nombre artículo: ', producto[0], 'Sección: ', producto[2])
 
 # 5. Cerrar puntero
 mi_conexion.commit()
